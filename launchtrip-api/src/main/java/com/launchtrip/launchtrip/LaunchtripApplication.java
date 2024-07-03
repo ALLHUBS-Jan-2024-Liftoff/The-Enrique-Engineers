bdbb21530f8b3bc23db68a2bc757a60b07a3270f
halrobison <halrobisoncreative@gmail.com>
2024-07-02 19:52:32 -0500

    backend setup

---
+++ launchtrip-api/src/main/java/com/launchtrip/launchtrip/LaunchtripApplication.java
@@ -0,0 +1,13 @@
+package com.launchtrip.launchtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchtripApplication.class, args);
+	}
+
+}
