d442c7f3e748dc4235b9ada15ddf754edcdd3a52
halrobison <halrobisoncreative@gmail.com>
2024-07-28 16:10:43 -0500

    changed LocationsRowItem from state to usState - fixed display error

--- launchtrip-ui/src/components/locations/LocationsRowItem.jsx
+++ launchtrip-ui/src/components/locations/LocationsRowItem.jsx
@@ -5,7 +5,7 @@ export const LocationsRowItem = ({ location, deleteLocation }) => {
     <tr key={location.id}>
       <th scope="row">{location.id}</th>
       <td>{location.name}</td>
-      <td>{location.state}</td>
+      <td>{location.usState}</td>
       <td>{location.country}</td>
       <td>{location.postcode}</td>
       <td>{location.categories.join(", ")}</td>
